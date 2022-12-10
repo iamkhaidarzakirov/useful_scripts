a26dc4bfce357bbac3494133c8db1f9864938c5a
iamkhaidarzakirov <khaidarzakirov@gmail.com>
2022-12-10 11:21:31 +0300

    I started learning parsing course and save that little useful script

---
+++ xl_sheet_to_csv/main.py
@@ -0,0 +1,9 @@
+from openpyxl import load_workbook
+import pandas as pd
+# Getting all sheets from Excel file. Converting to csv format and saving each sheet as new file
+wb = load_workbook(r'C:\Users\khaid\Downloads\data.xlsx')
+sheets_titles = wb.sheetnames
+print(sheets_titles)
+for title in sheets_titles:
+    df = pd.read_excel(r'C:\Users\khaid\Downloads\data.xlsx', sheet_name=f'{title}')
+    df.to_csv(f'{title}', header=True)
